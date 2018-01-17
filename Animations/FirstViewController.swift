e107f98b656c45953a0de82ea709da729f6d1b44
Admin <admin@admins-mac.local>
2018-01-17 19:50:20 +0200

    First Animations implemented

--- Animations/FirstViewController.swift
+++ Animations/FirstViewController.swift
@@ -9,17 +9,132 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    
+    
+    
+    @IBOutlet weak var Text_1: UITextView!
+    @IBOutlet weak var Text_2: UITextView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    let animations = ["Grow","Fade","Move"]
+    
+    var size  = CGSize()
+    var firstFieldCenter = CGPoint()
+    var secondFieldCenter = CGPoint()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        segmentControl.addTarget(self, action: #selector(self.changeAnimation), for: .valueChanged)
+        
+        
+        segmentControl.removeAllSegments()
+        
+        for i in 0..<animations.count {
+            segmentControl.insertSegment(withTitle: animations[i], at: i, animated: true)
+        }
+        segmentControl.selectedSegmentIndex = 0
+        
+        size  = Text_1.frame.size
+        firstFieldCenter = Text_1.center
+        secondFieldCenter = Text_2.center
+        
+        
+        //            // Animate the button
+        //            let currentButtonCenter = compressBtn.center
+        //            compressBtn.center.x = 500
+        //            compressBtn.alpha = 0.1
+        //
+        //            UIView.animate(withDuration: 2, animations: {
+        //                self.compressBtn.center = currentButtonCenter
+        //
+        //            })
+        //
+        //
+        //            UIView.animate(withDuration: 4, animations: {
+        //                self.compressBtn.alpha = 1
+        //
+        //            })
+        //
+        //            // Make the corners round because it's ugly.
+        //            compressBtn.layer.cornerRadius = 20
+        Text_1.layer.cornerRadius = 20
+        Text_2.layer.cornerRadius = 20
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    @objc func changeAnimation() {
+        
+        let animationChoosen = animations[segmentControl.selectedSegmentIndex]
+        switch animationChoosen {
+        case "Grow":
+            growTextField()
+        case "Fade":
+            fadeTextField()
+        case "Move":
+            moveTextField()
+        default:
+            print("Still not supported yet!")
+        }
     }
-
-
+    
+    @objc func growTextField() {
+        // Animate the fields.
+        
+        Text_1.frame.size.width = 0.0
+        Text_1.frame.size.height = 0.0
+        
+        UIView.animate(withDuration: 1, animations: {
+            self.Text_1.frame.size = self.size
+            
+        })
+        
+        Text_2.frame.size.width = 0.0
+        Text_2.frame.size.height = 0.0
+        
+        UIView.animate(withDuration: 1, animations: {
+            self.Text_2.frame.size = self.size
+            
+        })
+    }
+    
+    
+    
+    @objc func fadeTextField(){
+        Text_1.alpha = 0
+        UIView.animate(withDuration: 1.5, animations: {
+            self.Text_1.alpha = 1
+            
+        })
+        
+        Text_2.alpha = 0
+        UIView.animate(withDuration: 1.5, animations: {
+            self.Text_2.alpha = 1
+            
+        })
+    }
+    
+    @objc func moveTextField(){
+        Text_1.center.x = -500
+        UIView.animate(withDuration: 1, animations: {
+            self.Text_1.center = self.firstFieldCenter
+            
+        })
+        
+        Text_2.center.y = 800
+        UIView.animate(withDuration: 1, animations: {
+            self.Text_2.center = self.secondFieldCenter
+            
+        })
+    }
+    
+    
 }
 
--- Animations/SecondViewController.swift
+++ Animations/SecondViewController.swift
@@ -8,16 +8,90 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController  {
 
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    
+    var timer = Timer()
+    let animations = ["Ball","Empty"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        segmentControl.addTarget(self, action: #selector(self.changeAnimation), for: .valueChanged)
+        
+        
+        // Add the animations to the segment control.
+        segmentControl.removeAllSegments()
+        for i in 0..<animations.count {
+            segmentControl.insertSegment(withTitle: animations[i], at: i, animated: true)
+        }
+        segmentControl.selectedSegmentIndex = 0
+
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func changeAnimation() {
+        
+        timer.invalidate()
+        let remainingCircles = view.subviews
+        for each in remainingCircles {
+            if (each.tag == 1){
+                each.removeFromSuperview()
+            }
+        }
+        
+        let animationChoosen = animations[segmentControl.selectedSegmentIndex]
+        switch animationChoosen {
+        case "Ball":
+            // Start the timer for the circle animation
+             timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.circleAnimation), userInfo: nil, repeats: true)
+            timer.fire()
+        default:
+            print("Still not supported yet!")
+        }
+        
+        
+    }
+    
+    
+    @objc func circleAnimation()
+    {
+        // Initialize the circle.
+        let circleContainer = UIView()
+        circleContainer.frame.size = CGSize(width: 20.0, height: 20.0)
+        circleContainer.tag = 1
+        
+        
+        // Center it in the view.
+        circleContainer.center.x = view.center.x - view.frame.size.width + 5
+        circleContainer.center.y = view.center.y
+        
+        // To make it a circle
+        circleContainer.layer.cornerRadius = 10
+        
+        // Generate a random color.
+        circleContainer.backgroundColor = UIColor(red:CGFloat(drand48()) , green:CGFloat(drand48()) , blue: CGFloat(drand48()), alpha: 1)
+        
+        // Add to the top level view.
+        view.addSubview(circleContainer)
+        view.layoutIfNeeded()
+        
+        // Animate then remove it on finish
+        UIView.animate(withDuration: 5, delay: 0, options: .curveEaseIn, animations:{
+            circleContainer.center = CGPoint(x: self.view.bounds.maxX , y: self.view.center.y)
+        }, completion: { (fin) in
+            if (fin) {
+                circleContainer.removeFromSuperview()
+            }
+        })
+        
+        
     }
 
 
