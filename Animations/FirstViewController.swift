992691dbdc941125367bfea2769b48208911a1b0
Admin <admin@admins-mac.local>
2018-01-17 17:43:48 +0200

    Initial Commit

---
+++ Animations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Animations
+//
+//  Created by Admin on 1/17/18.
+//  Copyright © 2018 ahmednader. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
